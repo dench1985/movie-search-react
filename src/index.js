7da9e670d06c93fdc48e9425f4017e1aaa4c33c9
dench1985 <z_dien@mail.ru>
2024-09-17 21:39:51 +0300

    fist commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import MoviesDetails from './components/MovieDetails';
 import Favorites from './components/Favorites';
 import "./style.css"
 
+const GITHUB_NAME = "movie-search-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <BrowserRouter >
-    <Routes>
+    <Routes path = {GITHUB_NAME}>
       <Route path="/" element={<App/>} />
       <Route  path="/:id" element={<MoviesDetails/>} />
     </Routes>
